48602e8dc6628c5b3af37f333d312ced7c6cb035
Peter Stakoun <peter@stakoun.com>
2017-07-15 13:08:43 -0400

    Update Firebase integration

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import * as firebase from 'firebase';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+
+var config = {
+  apiKey: "AIzaSyDISmmf3W3F_1pAhcZw804Zny7w2ApYjJ8",
+  databaseURL: "https://decentralizedps.firebaseio.com",
+  authDomain: "decentralizedps.firebaseapp.com",
+  storageBucket: "decentralizedps.appspot.com"
+};
+firebase.initializeApp(config);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();

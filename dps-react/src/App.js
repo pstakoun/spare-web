3e26e09b4ad9ef97c2b3b1d971b30a2fd154e30b
Peter Stakoun <peter@stakoun.com>
2017-07-15 01:16:51 -0400

    Update UI

--- dps-react/src/App.js
+++ dps-react/src/App.js
@@ -1,19 +1,25 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Grid, Row, Col, Panel, ButtonGroup, Button } from 'react-bootstrap';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Grid>
+        <Row>
+          <h1>DPS</h1>
+        </Row>
+        <Row>
+          <Col lg={12}>
+            <Panel>
+              <ButtonGroup>
+                <Button>Log In</Button>
+                <Button>Register</Button>
+              </ButtonGroup>
+            </Panel>
+          </Col>
+        </Row>
+      </Grid>
     );
   }
 }

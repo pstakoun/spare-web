59023f546de8a1117d43148997e3c52d7a485b12
Shaolin Zhang <shaolin@shaolinzhang.com>
2017-07-16 01:44:22 -0400

    Added css for login

--- src/Login.js
+++ src/Login.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Button, Form, FormControl, FormGroup, Panel, Tab, Tabs } from 'react-bootstrap';
 import * as firebase from 'firebase';
+import './App.css';
 
 class Login extends Component {
   handleLogin(event) {
@@ -31,50 +32,49 @@ class Login extends Component {
 
   render() {
     return (
-      <Panel>
+      <Panel className="panel panel-login panel-heading">
         <Tabs defaultActiveKey={1}>
-          <Tab title="Log In" eventKey={1}>
-            <Form onSubmit={this.handleLogin}>
+          <Tab className="panel-heading" title="Log In" eventKey={1}>
+            <Form className="panel-body" onSubmit={this.handleLogin}>
               <FormGroup>
-                Email
-                <FormControl type="email" />
+                <FormControl className="form-control" type="email" placeholder="Email Address"/>
               </FormGroup>
               <FormGroup>
-                Password
-                <FormControl type="password" />
+                <FormControl className="form-control" type="password" placeholder="Password"/>
               </FormGroup>
               <FormGroup>
-                <Button type="submit">
+                <Button className="form-control btn btn-login" type="submit">
                   Log In
                 </Button>
               </FormGroup>
+              <center><p>
+                New to SPARE? Sign Up.
+              </p></center>
+              <center><p className="forgot-password">
+                Forgot Password?
+              </p></center>
             </Form>
           </Tab>
-          <Tab title="Register" eventKey={2}>
+          <Tab className="panel-heading" title="Register" eventKey={2}>
             <Form onSubmit={this.handleRegister}>
               <FormGroup>
-                First Name
-                <FormControl type="text" />
+                <FormControl className="form-control" type="text" placeholder="First Name" />
               </FormGroup>
               <FormGroup>
-                Last Nane
-                <FormControl type="text" />
+                <FormControl className="form-control" type="text" placeholder="Last Name" />
               </FormGroup>
               <FormGroup>
-                Email
-                <FormControl type="email" />
+                <FormControl className="form-control" type="email" placeholder="Email Address" />
               </FormGroup>
               <FormGroup>
-                Password
-                <FormControl type="password" />
+                <FormControl className="form-control" type="password" placeholder="Password" />
               </FormGroup>
               <FormGroup>
-                Confirm Password
-                <FormControl type="password" />
+                <FormControl className="form-control" type="password" placeholder="Confirm Password" />
               </FormGroup>
               <FormGroup>
-                <Button type="submit">
-                  Register
+                <Button className="form-control btn btn-register" type="submit">
+                  Register Now
                 </Button>
               </FormGroup>
             </Form>

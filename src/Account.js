53fe8d92ee103e592baca715874ebdcde1669f2c
Peter Stakoun <peter@stakoun.com>
2017-07-18 18:21:10 -0400

    Update account

--- src/Account.js
+++ src/Account.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Col, Nav, NavItem, Row } from 'react-bootstrap';
 import * as firebase from 'firebase';
 import Preferences from './Preferences';
+import Profile from './Profile';
 
 class Account extends Component {
   render() {
@@ -15,6 +16,7 @@ class Account extends Component {
           </Nav>
         </Col>
         <Col sm={9}>
+          <Profile />
           <Preferences />
         </Col>
       </Row>

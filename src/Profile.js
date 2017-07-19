f777516c7c658f7ec55c432ab3bfd7ce4deea645
Shaolin Zhang <shaolin@shaolinzhang.com>
2017-07-19 18:38:13 -0400

    Added profile

--- src/Profile.js
+++ src/Profile.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
-import { Button, Form, FormGroup } from 'react-bootstrap';
+import ReactDOM from 'react-dom';
+import { Button, Form, FormGroup, FormControl } from 'react-bootstrap';
 import * as firebase from 'firebase';
+import './App.css';
 
 class Profile extends Component {
   handleLogout(event) {
@@ -8,15 +10,49 @@ class Profile extends Component {
     firebase.auth().signOut();
   }
 
+  handleInfoChange(event) {
+    event.preventDefault();
+
+    var firstName = ReactDOM.findDOMNode(this.refs.userFirstName).value.trim();
+    var lastName = ReactDOM.findDOMNode(this.refs.userLastName).value.trim();
+    var email = ReactDOM.findDOMNode(this.refs.userEmail).value.trim();
+    var phone = ReactDOM.findDOMNode(this.refs.userPhoneNum).value;
+
+    firebase.database().ref('users/' + firebase.auth().currentUser.uid).set({
+        fname: firstName,
+        lname: lastName,
+        email: email,
+        phone: phone,
+    });
+  }
+
   render() {
     return (
-      <Form onSubmit={this.handleLogout}>
-        <FormGroup>
-          <Button type="submit">
-            Log Out
-          </Button>
-        </FormGroup>
-      </Form>
+        <Form className="panel-body" onSubmit={this.handleInfoChange.bind(this)}>
+          <center><h4>User Profile</h4></center>
+          <FormGroup>
+            <FormControl ref="userFirstName" placeholder="First Name"/>
+          </FormGroup>
+          <FormGroup>
+            <FormControl ref="userLastName" placeholder="Last Name"/>
+          </FormGroup>
+          <FormGroup>
+            <FormControl ref="userEmail" type="email" placeholder="Email Address for Correspondence"/>
+          </FormGroup>
+          <FormGroup>
+            <FormControl ref="userPhoneNum" placeholder="Phone Number"/>
+          </FormGroup>
+          <FormGroup>
+            <Button className="form-control btn" type="submit">
+              Submit Info
+            </Button>
+          </FormGroup>
+          <FormGroup onSubmit={this.handleLogout}>
+            <Button type="submit">
+              Log Out
+            </Button>
+          </FormGroup>
+        </Form>
     );
   }
 }

c52e147f97f0c2a24cd8deba53fe19cf47a10287
Peter Stakoun <peter@stakoun.com>
2017-07-22 14:56:38 -0400

    Update space list

--- src/Space.js
+++ src/Space.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
+import { Panel } from 'react-bootstrap';
 
 class Space extends Component {
   render() {
     return (
-      <img src={this.props.space.photoURL} style={{ width: `100px` }} />
+      <Panel>
+        <img src={this.props.space.photoURL} className="img-responsive center-block" />
+      </Panel>
     );
   }
 }

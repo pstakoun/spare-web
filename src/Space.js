c34ea18b8783e5b4c0a155a4f7d616d5663615c6
Peter Stakoun <peter@stakoun.com>
2017-07-28 01:48:29 -0400

    Update overlay

--- src/Space.js
+++ src/Space.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
-import { Panel } from 'react-bootstrap';
+import { Button, Panel } from 'react-bootstrap';
 
 class Space extends Component {
   render() {
     return (
       <Panel>
         <img src={this.props.space.photoURL} className="img-responsive center-block" />
+		<Button>Go</Button>
       </Panel>
     );
   }

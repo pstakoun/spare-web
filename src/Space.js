87684b7ac6d5b194d8938f8b4ab90a075d184951
Shaolin Zhang <shaolin@shaolinzhang.com>
2017-07-28 22:17:11 -0400

    Kinda made it work

--- src/Space.js
+++ src/Space.js
@@ -1,16 +1,30 @@
 import React, { Component } from 'react';
 import { Button, Panel } from 'react-bootstrap';
+import * as firebase from 'firebase';
 
 class Space extends Component {
-  handleGo(e) {
-    this.props.selectSpace(this.props.space);
+
+  handleImg() {
+    firebase.storage().refFromURL(this.props.space.photoURL).getDownloadURL().then(function(url) {
+      document.querySelector('img').src = url;
+    }).catch(function(error) {
+      console.error(error);
+    });
   }
 
+  handleGo(e) {
+  }
+
+
+
   render() {
+
+    {this.handleImg()}
+
     return (
       <Panel>
-        <img src={this.props.space.photoURL} className="img-responsive center-block" />
-		<Button onClick={this.handleGo.bind(this)}>Go</Button>
+        <img className="img-responsive center-block" />
+		    <Button onClick={this.handleGo.bind(this)}>Go</Button>
       </Panel>
     );
   }

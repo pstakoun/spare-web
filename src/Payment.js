2a6382ee0e75a45d38514d2ab2e63296cfa011f3
Peter Stakoun <peter@stakoun.com>
2017-07-29 20:43:02 -0400

    Add payment

--- src/Payment.js
+++ src/Payment.js
@@ -1,9 +1,15 @@
 import React, { Component } from 'react';
+import { StripeProvider, Elements } from 'react-stripe-elements';
+import Checkout from './Checkout';
 
 class Payment extends Component {
   render() {
     return (
-      <h2>Payment</h2>
+      <StripeProvider apiKey="pk_test_12345">
+        <Elements>
+          <Checkout />
+        </Elements>
+      </StripeProvider>
     );
   }
 }

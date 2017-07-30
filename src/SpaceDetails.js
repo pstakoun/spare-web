55ba3832b9d3f8e06e98e5a5109bc507425523c1
Shaolin Zhang <shaolin@shaolinzhang.com>
2017-07-30 00:42:14 -0400

    Added details

--- src/SpaceDetails.js
+++ src/SpaceDetails.js
@@ -12,11 +12,31 @@ class SpaceDetails extends Component {
   }
 
   render() {
+
+    let has_lock_local, has_insurance_local, all_access_local, climate_control_local = null;
+
+    if (this.props.space.has_lock) {
+      has_lock_local = [<p> Has Security Locks </p>];
+    }
+    if (this.props.space.has_insurance) {
+      has_insurance_local = [<p> Has Insurance Plans </p>];
+    }
+    if (this.props.space.all_access) {
+      all_access_local = [<p> 24/7 Access </p>];
+    }
+    if (this.props.space.climate_control) {
+      climate_control_local = [<p> Has Climate Control System </p>];
+    }
+
     {this.handleImg()}
     return (
       <Panel>
         <img className="img-responsive center-block" />
-		<p>{this.props.space.address}</p>
+		    <p>{this.props.space.address}</p>
+        {has_lock_local}
+        {has_insurance_local}
+        {all_access_local}
+        {climate_control_local}
       </Panel>
     );
   }

c209d7440f7e104b4121db9e872d664ab52daf3b
Peter Stakoun <peter@stakoun.com>
2017-08-03 00:22:23 -0400

    Logout redirect

--- src/Preferences.js
+++ src/Preferences.js
@@ -1,16 +1,24 @@
 import React, { Component } from 'react';
 import { Button, Form, FormGroup } from 'react-bootstrap';
+import { Redirect } from 'react-router-dom';
 import * as firebase from 'firebase';
 
 class Preferences extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { loggedOut: false };
+  }
+
   handleLogout(event) {
     event.preventDefault();
     firebase.auth().signOut();
+    this.setState({ loggedOut: true });
   }
 
   render() {
     return (
-      <Form onSubmit={this.handleLogout} className="logout">
+      <Form onSubmit={this.handleLogout.bind(this)} className="logout">
+        {this.state.loggedOut && <Redirect to='/' push />}
         <FormGroup>
           <Button type="submit">
             Log Out
